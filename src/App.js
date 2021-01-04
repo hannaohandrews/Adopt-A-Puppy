e3bb5797e73f513e8edf3c4db884186fceef1c04
Hanna Oh <63070798+hannaohandrews@users.noreply.github.com>
2021-01-04 09:47:15 -0800

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Dog",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      name: "Hohoho",
-      animal: "Dog",
-      breed: "Golden",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root")); //where App is rendering => root //
+render(<App />, document.getElementById("root")); //where App is rendering => root //
+
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Dog",
+//       breed: "Mixed",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Hohoho",
+//       animal: "Dog",
+//       breed: "Golden",
+//     }),
+//   ]);
