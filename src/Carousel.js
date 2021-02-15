879e7668596ef2bb1f6048696fc38b90a168ad40
Hanna Oh <63070798+hannaohandrews@users.noreply.github.com>
2021-02-15 11:50:26 -0800

    modifiying carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,32 +1,34 @@
 import React from "react";
 
 class Carousel extends React.Component {
-  //public class property
   state = {
     photos: [],
     active: 0,
   };
-
   static getDerivedStateFromProps({ media }) {
     let photos = ["http://placecorgi.com/600/600"];
 
     if (media.length) {
       photos = media.map(({ large }) => large);
     }
+
     return { photos };
   }
+  // whenever you are doing an event listener , always use arrow function b.c it will guarnettee the "this"
   handleIndexClick = (event) => {
     this.setState({
       //+ Unary Plus  => truns string into a number
+      // same thing as parseInt
       active: +event.target.dataset.index,
     });
   };
+
   render() {
     const { photos, active } = this.state;
 
     return (
       <div className="carousel">
-        <img src={photos[active]} alt="animal"></img>
+        <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
             //eslint-disable-next-line
@@ -34,6 +36,7 @@ class Carousel extends React.Component {
               key={photo}
               onClick={this.handleIndexClick}
               data-index={index}
+              src={photo}
               className={index === active ? "active" : ""}
               alt="animal-thumnail"
             />
